68e7f8355bbaad5f251e9667ab18666f3fe0cb6c
Maksim <maxim.kudryavtsev@omega-r.com>
2021-04-11 23:10:08 +0300

    Добавил кнопку начать заново

--- src/game.ts
+++ src/game.ts
@@ -33,7 +33,7 @@ export class Game {
         this.subscribeMovementListeners();
     }
 
-    restart(options: Partial<GameOptions>) {
+    restart(options?: Partial<GameOptions>) {
         this.clear();
         this.options = {
             ...this.options,
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,7 @@
 import { Game } from "./game";
 import { Images } from "./Images";
 
-const imagesSrc = [
-    'images/1.jpg',
-    'images/2.jpg',
-    'images/3.jpg',
-    'images/4.jpg',
-    'images/5.jpg',
-]
+const imagesSrc = require('../public/images.json');
 
 function init() {
     const backlog = document.getElementById("backlog");
@@ -16,15 +10,17 @@ function init() {
     const imagesContainer = document.getElementById("images");
     const sideSizeInput = document.getElementById("sideSize") as HTMLInputElement;
     const submit = document.getElementById("submit");
-    if (backlog && playground && result && imagesContainer && submit) {
-        const images = new Images(imagesContainer, imagesSrc);
-        const game = new Game({
-            sideSize: 5,
+    const reset = document.getElementById("reset");
+    if (backlog && playground && result && imagesContainer && submit && reset) {
+        const images = new Images(imagesContainer, imagesSrc.images);
+        const initialOptions = {
+            sideSize: 4,
             containers: {
                 result, playground, backlog
             },
             imageSrc: images.getCurrentSrc()
-        })
+        }
+        const game = new Game(initialOptions)
         game.start();
         images.subscribeListeners(src => game.restart({
             imageSrc: src
@@ -32,6 +28,10 @@ function init() {
         submit.onclick = () => game.restart({
             sideSize: Number(sideSizeInput.value)
         })
+        reset.onclick = () => {
+            game.restart(initialOptions);
+            sideSizeInput.value = String(initialOptions.sideSize)
+        };
     }
 }
 
